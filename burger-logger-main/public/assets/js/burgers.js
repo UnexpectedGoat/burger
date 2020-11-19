e5166abace8aab7224644b8a92fbd59445a6ab35
Timothy Phillips <elrenmazuo@gmail.com>
2020-11-19 14:10:28 -0800

    update

---
+++ burger-logger-main/public/assets/js/burgers.js
@@ -0,0 +1,52 @@
+$(function () {
+    $(".change-eaten").on("click", function (event) {
+        var id = $(this).data("id");
+        var isEaten = $(this).data("iseaten");
+
+        var newEatenState = {
+            eaten: isEaten
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newEatenState
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#ca").val().trim(),
+            eaten: $("[name=eaten]:checked").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
+});
